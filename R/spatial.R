f00bcd72e631aa6e21c20d72e039bf00812fb937
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-06-28 21:22:24 -0700

    spdat

--- R/spatial.R
+++ R/spatial.R
@@ -1,80 +1,37 @@
 
 
 setMethod("predict", signature("Rcpp_QueftsModel"), 
-function(object, data, filename="", overwrite=FALSE, wopt=list(), ...)  {
+function(object, supply, yatt, filename="", overwrite=FALSE, wopt=list(), ...)  {
 
-	preds <- list(...)
-	if (length(preds) == 0) {
-		return(run(object))
+	stopifnot(inherits(supply, "SpatRaster"))
+	stopifnot(inherits(yatt, "SpatRaster"))
+	stopifnot(nlyr(supply) == 3)
+	stopifnot(terra::nlyr(yatt) == 1)
+	nms <- toupper(substr(names(supply), 1, 1))
+	if (!all(nms1 == c("N", "P", "K"))) {
+		stop("the names of 'supply' must start with 'N', 'P', 'K'")
 	}
+	names(supply) <- c("Ns", "Ps", "Ks")
+	terra::compareGeom(supply, yatt, lyrs=FALSE, crs=TRUE, warncrs=TRUE, ext=TRUE, rowcol=TRUE, res=FALSE)
+
+	terra::readStart(supply)
+	on.exit(terra::readStop(supply))
+	terra::readStart(yatt)
+	on.exit(terra::readStop(yatt))
+
+	out <- terra::rast(yatt)
+	names(out) <- "yield"
 	
-	# use collection?
-	e <- sapply(preds, function(i) as.vector(terra::ext(i)))
-	if (!all(apply(e == e[,1], 1, all))) {
-		stop("extents are not equal")
-	}
-	r <- sapply(preds, terra::res)
-	if (!(all(apply(r == r[,1], 1, all)))) {
-		stop("resolutions are not equal")	
-	}
-
-	nms <- trimws(names(preds))
-	if (is.null(nms) | any(nms=="")) stop ("SpatRaster arguments must be named")
-	# check if nms in model
-
-	nlyrs <- sapply(preds, terra::nlyr)
-	if (!all(nlyrs %in% c(1, 12))) stop("all SpatRaster objects must have either 1 or 12 layers")
-	di <- nlyrs == 12
-	dpreds <- preds[di]
-	spreds <- preds[!di]
-		
-	out <- rast(preds[[1]])
-	nms <- object$names()
-	nlyr(out) <- length(nms)
-	if (length(wopt$names) != length(nms)) {
-		names(out) <- nms
-		wopt$names <- NULL
-	}
-	## depends on future version of terra
-	#if (object$lim_fact) {
-    #	levels(out) <- object$parameter_names
-	#}
-	nc <- ncol(out)
-	lapply(preds, terra::readStart)
-	
+	nc <- ncol(yatt)
 	b <- terra::writeStart(out, filename, overwrite, wopt)
 	for (i in 1:b$n) {
-		removePredictor(object, "ALL")
-		
-		if (length(dpreds) > 0) {
-			v <- lapply(dpreds, function(r) {
-				as.vector(t(terra::readValues(r, b$row[i], b$nrows[i], 1, nc, TRUE)))
-			})
-			vv <- do.call(cbind, v)
-			names(vv) <- nms[di]
-			dynamicPredictors(object) <- vv
-		}
-		if (length(spreds) > 0) {
-			v <- lapply(spreds, function(r) {
-				as.vector(t(terra::readValues(r, b$row[i], b$nrows[i], 1, nc, TRUE)))
-			})
-			vv <- do.call(cbind, v)
-			names(vv) <- nms[!di]
-			staticPredictors(object) <- vv
-		}
-		
-		eco <- run(object)
-		removePredictor(object, "ALL")
-		if (nlyr(out) > 1) {
-			eco <- as.vector(matrix(eco, ncol=nlyr(out), byrow=TRUE))
-		}
-		terra::writeValues(out, eco, b$row[i], b$nrows[i])
+		vs <- terra::readValues(supply, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
+		vy <- terra::readValues(yatt, b$row[i], b$nrows[i], 1, nc)
+		v <- run(object, vs, vy)
+		terra::writeValues(out, v, b$row[i], b$nrows[i])
 	}
-	object$out <- 0
-	lapply(preds, terra::readStop)
 	out <- terra::writeStop(out)
 	return(out)
 }
 )
 
-# SpatRaster::setCategories(unsigned layer, std::vector<std::string> labs, std::vector<double> levs
