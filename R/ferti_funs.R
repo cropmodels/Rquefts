dc5ecbf6a26cdf9227521957400bcc5069ced959
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-06-29 09:54:55 -0700

    na price

--- R/ferti_funs.R
+++ R/ferti_funs.R
@@ -18,29 +18,31 @@ nutrientRates <- function(supply, treatment) {
 # minimize total cost of a fertilizer treatment
 # based on a function by Pieter Pypers
 fertApp <- function(nutrients, fertilizers, price, exact=TRUE, retCost=FALSE){
+
+	stopifnot(length(price) == nrow(fertilizers))
 	name <- fertilizers$name
 	supply <- t(as.matrix(fertilizers[,-1,drop=FALSE]))
 	treatment <- as.matrix(nutrients)
 
 	result <- matrix(nrow=ncol(supply), ncol=nrow(treatment))
-	stopifnot(length(price) == nrow(fertilizers))
-	treatment <- treatment * 100
-	for (i in 1:nrow(treatment)) {
-		if (exact) {
-			solution <- limSolve::linp(E=supply, F=treatment[i,], Cost=price)
-		} else {
-			solution <- limSolve::linp(G=supply, H=treatment[i,], Cost=price)		
-		}
-		if (solution$IsError) {
-			result[,i] <- NA
-		} else { 	
-			result[,i] <- solution$X
+	colnames(result) <- rownames(nutrients)
+	if (!any(is.na(price))) {
+		treatment <- treatment * 100
+		for (i in 1:nrow(treatment)) {
+			if (exact) {
+				solution <- limSolve::linp(E=supply, F=treatment[i,], Cost=price)
+			} else {
+				solution <- limSolve::linp(G=supply, H=treatment[i,], Cost=price)		
+			}
+			if (solution$IsError) {
+				result[,i] <- NA
+			} else { 	
+				result[,i] <- solution$X
+			}
 		}
 	}
-	
-	colnames(result) <- rownames(nutrients)
 	if (retCost) {
-			
+		r <- colSums(result * price)
 	} else {
 		r <- data.frame(name, result)
 		#r[apply(r[,-1,drop=FALSE], 1, function(i) !all(i==0)), ]	
